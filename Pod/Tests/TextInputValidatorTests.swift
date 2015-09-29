8a9e231ef009589bb0a1d5a30bb7e386518313fd
Elvis Nuñez <elvisnunez@me.com>
2015-09-29 07:33:00 +0200

    Add NameInputValidatorTests

--- Pod/Tests/TextInputValidatorTests.swift
+++
@@ -1,5 +0,0 @@
-import UIKit
-import XCTest
-
-class TextInputValidatorTests: XCTestCase {
-}
---
+++ Source/NameInputValidator.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+public struct NameInputValidator: Validatable {
+    var validation: Validation
+
+    public init(validation: Validation) {
+        self.validation = validation
+    }
+
+    public func validateString(string: String) -> Bool {
+        return self.validateReplacementString(nil, usingFullString: string, inRange: nil)
+    }
+
+    func validateReplacementString(replacementString: String?, usingFullString fullString: String?, inRange range: NSRange?) -> Bool {
+        let baseInputValidator = InputValidator(validation: self.validation)
+        var valid = baseInputValidator.validateReplacementString(replacementString, usingFullString: fullString, inRange: range)
+        if valid {
+            if let replacementString = replacementString {
+                let letterCharacterSet = NSCharacterSet.letterCharacterSet()
+                let whitespaceCharacterSet = NSCharacterSet.whitespaceCharacterSet()
+                let delimiterCharacterSet = NSCharacterSet(charactersInString: "-")
+                let stringSet = NSCharacterSet(charactersInString: replacementString)
+                let allowsString = (
+                    letterCharacterSet.isSupersetOfSet(stringSet) ||
+                    whitespaceCharacterSet.isSupersetOfSet(stringSet) ||
+                    delimiterCharacterSet.isSupersetOfSet(stringSet)
+                )
+                valid = allowsString
+            }
+        }
+
+        return valid
+    }
+}
