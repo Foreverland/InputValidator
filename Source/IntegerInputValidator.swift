4f0e019b3e882fa4698531e9edfc51a992ea8f0b
Elvis Nuñez <elvisnunez@me.com>
2015-09-29 11:00:45 +0200

    Improve access control

--- Source/IntegerInputValidator.swift
+++ Source/IntegerInputValidator.swift
@@ -11,7 +11,7 @@ public struct IntegerInputValidator: Validatable {
         return self.validateReplacementString(nil, usingFullString: string, inRange: nil)
     }
 
-    func validateReplacementString(replacementString: String?, usingFullString fullString: String?, inRange range: NSRange?) -> Bool {
+    public func validateReplacementString(replacementString: String?, usingFullString fullString: String?, inRange range: NSRange?) -> Bool {
         let baseInputValidator = InputValidator(validation: self.validation)
         var valid = baseInputValidator.validateReplacementString(replacementString, usingFullString: fullString, inRange: range)
         if valid {
