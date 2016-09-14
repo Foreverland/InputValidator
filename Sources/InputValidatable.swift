7558cb21ec88d7328814748e8801bacbc9c60f1d
Elvis Nuñez <elvisnunez@me.com>
2016-09-14 23:58:02 +0200

    More migration

--- Sources/InputValidatable.swift
+++ Sources/InputValidatable.swift
@@ -6,15 +6,15 @@ public protocol InputValidatable {
 
     init(validation: Validation?)
 
-    func validateString(_ string: String) -> Bool
+    func validateString(string: String) -> Bool
 
     // This method is useful for partial validations, or validations where the final string is
     // in process of been completed. For example when entering characters into an UITextField.
-    func validateReplacementString(_ replacementString: String?, fullString: String?, inRange range: NSRange?) -> Bool
+    func validateReplacementString(replacementString: String?, fullString: String?, inRange range: NSRange?) -> Bool
 }
 
 extension InputValidatable {
-    public func validateString(_ string: String) -> Bool {
+    public func validateString(string: String) -> Bool {
         var valid = true
         if let validation = self.validation {
             valid = validation.validateString(string)
@@ -27,7 +27,7 @@ extension InputValidatable {
         return valid
     }
 
-    public func validateReplacementString(_ replacementString: String?, fullString: String?, inRange range: NSRange?) -> Bool {
+    public func validateReplacementString(replacementString: String?, fullString: String?, inRange range: NSRange?) -> Bool {
         var valid = true
         if let validation = self.validation {
             let evaluatedString = self.composedString(replacementString, fullString: fullString, inRange: range)
@@ -37,15 +37,15 @@ extension InputValidatable {
         return valid
     }
 
-    public func composedString(_ replacementString: String?, fullString: String?, inRange range: NSRange?) -> String {
+    public func composedString(replacementString: String?, fullString: String?, inRange range: NSRange?) -> String {
         var composedString = fullString ?? ""
 
-        if let replacementString = replacementString, let range = range {
-            let index = composedString.characters.index(composedString.startIndex, offsetBy: range.location)
+        if let replacementString = replacementString, range = range {
+            let index = composedString.startIndex.advancedBy(range.location)
             if range.location == composedString.characters.count {
-                composedString.insert(contentsOf: replacementString.characters, at: index)
+                composedString.insertContentsOf(replacementString.characters, at: index)
             } else {
-                composedString = (composedString as NSString).replacingCharacters(in: range, with: replacementString)
+                composedString = (composedString as NSString).stringByReplacingCharactersInRange(range, withString: replacementString)
             }
             return composedString
         }
