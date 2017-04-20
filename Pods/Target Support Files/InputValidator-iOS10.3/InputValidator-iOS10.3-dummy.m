65cc9b27cf1911398748a69ae95ad308471fc8ef
Elvis Nuñez <elvisnunez@me.com>
2017-04-20 10:43:33 +0200

    Include pods in repo

---
+++ Pods/Target Support Files/InputValidator-iOS10.3/InputValidator-iOS10.3-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_InputValidator_iOS10_3 : NSObject
+@end
+@implementation PodsDummy_InputValidator_iOS10_3
+@end
---
+++ Pods/Target Support Files/Validation-iOS9.0/Validation-iOS9.0-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Validation_iOS9_0 : NSObject
+@end
+@implementation PodsDummy_Validation_iOS9_0
+@end
