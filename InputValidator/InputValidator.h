195508c7a2b4926c5f64a999692151ef19b442f6
Elvis Nuñez <elvisnunez@me.com>
2017-07-19 15:08:47 +0200

    Remove CocoaPods

---
+++ InputValidator/InputValidator.h
@@ -0,0 +1,19 @@
+//
+//  InputValidator.h
+//  InputValidator
+//
+//  Created by Nuñez, Elvis on 19/07/2017.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InputValidator.
+FOUNDATION_EXPORT double InputValidatorVersionNumber;
+
+//! Project version string for InputValidator.
+FOUNDATION_EXPORT const unsigned char InputValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InputValidator/PublicHeader.h>
+
+
--- Podfile
+++
@@ -1,9 +0,0 @@
-use_frameworks!
-
-target 'Tests' do
-  pod 'InputValidator', path: "."
-end
-
-target 'Demo' do
-  pod 'InputValidator', path: "."
-end
